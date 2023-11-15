5d50f6f496e0b01831e0918ce04767609833f86a
SAI TEJA GILUKARA <78465749+saiteja12-g@users.noreply.github.com>
2023-11-14 21:11:21 -0500

    updated cpp files to fix errors

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -26,42 +26,30 @@ using namespace std::chrono_literals;
 
 using sharedFuture = rclcpp::Client<beginner_tutorials::srv::MsgModify>::SharedFuture;
 
-/**
- * @brief MinimalPublisher class implementation.
- * provides a simple implementation of the MinimalPublisher which 
- * defines the talker, service client interface.
- * 
- */
-
 class MinimalPublisher : public rclcpp::Node {
  public:
- /**
-  * @brief Construct a new Minimal Publisher object
-  * 
-  */
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+    // Declare parameters
     auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
     param_desc.description = "Set callback frequency.";
     this->declare_parameter("freq", 2.0, param_desc);
     auto param = this->get_parameter("freq");
     auto freq = param.get_parameter_value().get<std::float_t>();
     RCLCPP_DEBUG(this->get_logger(),
-                 "Parameter frequency is set to 2.0 hz");
+                 "The parameter freq is declared, set to 2.0 hz");
 
-    // creating a subscriber for tthe parameter
+    // Create subscriber for the parameters
     parameter_subscriber_ =
         std::make_shared<rclcpp::ParameterEventHandler>(this);
     auto parameterCallbackPtr =
         std::bind(&MinimalPublisher::parameter_callback, this, _1);
     parameterHandle_ = parameter_subscriber_->add_parameter_callback(
         "freq", parameterCallbackPtr);
-    
+
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    
+
     // Logging information
-    RCLCPP_DEBUG(this->get_logger(), "Publisher is Created");
+    RCLCPP_DEBUG(this->get_logger(), "Created Publisher");
     auto delta = std::chrono::milliseconds(static_cast<int>((1000 / freq)));
     timer_ = this->create_wall_timer(
         delta, std::bind(&MinimalPublisher::timer_callback, this));
@@ -69,7 +57,7 @@ class MinimalPublisher : public rclcpp::Node {
     client = this->create_client<beginner_tutorials::srv::MsgModify>("modify_msg");
 
     // Logging information
-    RCLCPP_DEBUG(this->get_logger(), "Client created");
+    RCLCPP_DEBUG(this->get_logger(), "Created Client");
     while (!client->wait_for_service(1s)) {
       // Log for interruption
       if (!rclcpp::ok()) {
@@ -79,30 +67,33 @@ class MinimalPublisher : public rclcpp::Node {
       // Logging information till waiting for the server
       RCLCPP_WARN(
           rclcpp::get_logger("rclcpp"),
-          "NO SERVICE AVAILABLE, RUN SERVER TO REFLECT THE MODIFICATIONS");
+          "RUN SERVER TO REFLECT CHANGES");
     }
   }
 
  private:
-  
   std::string Message;
   rclcpp::Client<beginner_tutorials::srv::MsgModify>::SharedPtr client;
   std::shared_ptr<rclcpp::ParameterEventHandler> parameter_subscriber_;
   std::shared_ptr<rclcpp::ParameterCallbackHandle> parameterHandle_;
 
   void timer_callback() {
+    RCLCPP_INFO_STREAM_ONCE(this->get_logger(), "Node setup");
     auto message = std_msgs::msg::String();
     message.data = "Teja! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    // Log message when service is available
+    RCLCPP_INFO(this->get_logger(), "Publishing Node: '%s'",
+                message.data.c_str());
     publisher_->publish(message);
-  // Call service  at frequency of 10
+    // Call service at the frequency of 10
     if (count_ % 10 == 0) {
       call_service();
     }
     auto steady_clock = rclcpp::Clock();
     RCLCPP_DEBUG_STREAM_THROTTLE(this->get_logger(), steady_clock, 10000,
-                                  "Node running successfully");
+                                 "Node running successfully");
   }
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
@@ -132,7 +123,8 @@ class MinimalPublisher : public rclcpp::Node {
                 param.as_double());
     RCLCPP_WARN(this->get_logger(), "The base frequency has been changed");
 
-    RCLCPP_FATAL_EXPRESSION(this->get_logger(), param.as_double() == 0.0, "Frequency is set to zero, zero division error");
+    RCLCPP_FATAL_EXPRESSION(this->get_logger(), param.as_double() == 0.0,
+                            "Frequency is set to zero, zero division error");
     if (param.as_double() == 0.0) {
       RCLCPP_ERROR(this->get_logger(), "Frequency has not been changed.");
     } else {
@@ -144,7 +136,7 @@ class MinimalPublisher : public rclcpp::Node {
   }
 };
 
-int main(int argc, char * argv[]) {
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <beginner_tutorials/srv/msg_modify.hpp>
 #include <cstdlib>
 #include <iterator>
 #include <memory>
@@ -5,22 +6,20 @@
 #include <string>
 #include <vector>
 
-#include "beginner_tutorials/srv/msg_modify.hpp"
-
 using MsgModify = beginner_tutorials::srv::MsgModify;
 
-
-void add(const std::shared_ptr<MsgModify::Request> request, std::shared_ptr<MsgModify::Response> response) {
+void add(const std::shared_ptr<MsgModify::Request> request,
+         std::shared_ptr<MsgModify::Response> response) {
   response->c = "String has been modified to " + request->a + "-" + request->b;
 }
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   std::shared_ptr<rclcpp::Node> node =
-      rclcpp::Node::make_shared("MsgModify_server");
+      rclcpp::Node::make_shared("modify_msg_server");
 
   rclcpp::Service<MsgModify>::SharedPtr service =
-      node->create_service<MsgModify>("MsgModify", &add);
+      node->create_service<MsgModify>("modify_msg", &add);
 
   // Logging information to show when the server is started
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Server Started.. Modifying Msg");
